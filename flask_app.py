36fb7f5bec7e488f17e9714f6ee98ed944a41799
Anthony Wong <antawck@gmail.com>
2022-10-22 17:33:26 +0200

    param in function

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,6 @@ def main():
 	return 'manhwas_list'
 
 @app.route('/manhwas/<manhwa_name>')
-def show_manhwa():
+def show_manhwa(manhwa_name):
 	pages = os.listdir(f"manhwas/{manhwa_name}")
 	return render_template('report.html', pages = pages)
