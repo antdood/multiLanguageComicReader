98edbaf3e97ca7e995af83820e7891fb8baf5575
kaeeeeede <kaedewrites1@gmail.com>
2022-10-28 21:36:31 +0800

    fixed function naming error

--- flask_app.py
+++ flask_app.py
@@ -17,7 +17,7 @@ def show_chapters(manhwa_name):
 	return render_template(f'chapters.html', chapter_list = chapter_list, manhwa_name = manhwa_name)
 
 @app.route('/manhwas/<manhwa_name>/<chapter_name>')
-def show_chapters(manhwa_name, chapter_name):
+def show_chapter(manhwa_name, chapter_name):
 	eng_pages = os.listdir(f"{BASE_DIR}/static/manhwas/{manhwa_name}/{chapter_name}/eng")
 	kor_pages = os.listdir(f"{BASE_DIR}/static/manhwas/{manhwa_name}/{chapter_name}/kor")
 	return render_template(f'manhwa.html', eng_pages = eng_pages, kor_pages = kor_pages, manhwa_name = manhwa_name)
